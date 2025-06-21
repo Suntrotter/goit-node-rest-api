d05e43f6641d97421a661d6c831858012d09f316
Olena Deynega <suntrotter@gmail.com>
2025-06-21 17:26:00 +0300

    initial

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,26 @@
 import Joi from "joi";
 
-export const addContactSchema = Joi.object({
+export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^[\d\s\-().]+$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Phone format is invalid",
+    }),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
-  phone: Joi.string(),
-}).or("name", "email", "phone"); // принаймні одне поле має бути
+  phone: Joi.string()
+    .pattern(/^[\d\s\-().]+$/)
+    .messages({
+      "string.pattern.base": "Phone format is invalid",
+    }),
+});
+
+export const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean(),
+});
